799e1c66d7288b5ebaaef43f83da8e127e3fdefe
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-14 14:43:28 -0800

    Begin board and square classes

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log(`Hello from knights`);
+
+
+class Square {
+    constructor(xCoordinate, yCoordinate){
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    findPossibleMoves(){
+        // Write a function that outputs a list of all the possible squares a knight could move to from here
+
+        let xCoordinate = this.xCoordinate;
+        let yCoordinate = this.yCoordinate;
+
+
+
+    }
+
+}
+
+class Board{
+    constructor(){
+        this.contents = contents
+    }
+
+
+    knightMoves(startPosition, endPosition){
+        // Takes two arrays as arguments
+        // Each array is 2 numbers, the xCoordiante and the yCoordinate
+    }
+
+
+}
+
+
+// Need a function to populate the board with spaces
