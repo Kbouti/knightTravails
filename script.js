c1958c1d1945107206400d0d0eee7685d471c394
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-14 14:55:47 -0800

    Write createBoard function

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 console.log(`Hello from knights`);
 
-
 class Square {
-    constructor(xCoordinate, yCoordinate){
-        this.xCoordinate = xCoordinate;
-        this.yCoordinate = yCoordinate;
-    }
+  constructor(xCoordinate, yCoordinate) {
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+  }
 
-    findPossibleMoves(){
-        // Write a function that outputs a list of all the possible squares a knight could move to from here
+  findPossibleMoves() {
+    // Write a function that outputs a list of all the possible squares a knight could move to from here
 
-        let xCoordinate = this.xCoordinate;
-        let yCoordinate = this.yCoordinate;
-
-
-
-    }
-
+    let xCoordinate = this.xCoordinate;
+    let yCoordinate = this.yCoordinate;
+  }
 }
 
-class Board{
-    constructor(){
-        this.contents = contents
+class Board {
+  constructor() {
+    this.contents = this.createBoard();
+  }
+
+  createBoard() {
+    let board = [];
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i < 64; i++) {
+        board.push(new Square(x, y))
+      if (y < 8) {
+        y++;
+      }
+      if (y == 8) {
+        x++;
+        y = 0;
+      }
     }
-
-
-    knightMoves(startPosition, endPosition){
-        // Takes two arrays as arguments
-        // Each array is 2 numbers, the xCoordiante and the yCoordinate
-    }
-
-
+  }
+  
+  knightMoves(startPosition, endPosition) {
+    // Takes two arrays as arguments
+    // Each array is 2 numbers, the xCoordiante and the yCoordinate
+  }
 }
 
 
-// Need a function to populate the board with spaces
+let board1 = new Board;
